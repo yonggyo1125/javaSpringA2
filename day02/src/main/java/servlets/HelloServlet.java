4d36195510ccddcad9cc852a27721a224613e955
yonggyo1125 <yonggyo00@kakao.com>
2023-04-25 17:53:04 +0900

    수업 소스

--- day02/src/main/java/servlets/HelloServlet.java
+++ day02/src/main/java/servlets/HelloServlet.java
@@ -9,6 +9,6 @@ import java.io.IOException;
 public class HelloServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doGet(req, resp);
+
     }
 }
