fb674b5c9abebfd711b580c08c82dd53494bcf05
yonggyo1125 <yonggyo00@kakao.com>
2023-04-26 16:54:19 +0900

    수업 소스

--- ServletStudy/src/main/java/filters/Filter1.java
+++ ServletStudy/src/main/java/filters/Filter1.java
@@ -8,6 +8,8 @@ public class Filter1 implements Filter {
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain) throws IOException, ServletException {
         System.out.println("Filter1");
 
+        // 요청 URL에 매칭되는 다음 필터가 있는 경우 -> 다음 필터 호출
+        // 없는 경우는 서블릿의 요청 메서드가 호출 -> 응답
         filterChain.doFilter(request, response);
     }
 }
---
+++ ServletStudy/src/main/java/filters/Filter2.java
@@ -0,0 +1,12 @@
+package filters;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+public class Filter2 implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+
+    }
+}
