406177d3bba7987ab78191f1faba7ca78961a5ef
yonggyo1125 <yonggyo00@kakao.com>
2023-04-26 15:40:16 +0900

    수업 소스

--- ServletStudy/src/main/java/controllers/BoardServlet.java
+++ ServletStudy/src/main/java/controllers/BoardServlet.java
@@ -5,14 +5,15 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
 
-public class HelloServlet extends HttpServlet {
+public class BoardServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.setContentType("text/html;charset=UTF-8");
-       // resp.setCharacterEncoding("UTF-8");
-        PrintWriter out = resp.getWriter();
-        out.println("<h1>안녕하세요!</h1>");
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
     }
 }
