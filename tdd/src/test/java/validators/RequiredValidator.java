78d07ed7a55b8715e8fca7182d98de1a5ac487d4
yonggyo1125 <yonggyo00@kakao.com>
2023-05-08 08:30:28 +0900

    소스 업로드

--- tdd/src/test/java/validators/RequiredValidator.java
+++
@@ -1,9 +0,0 @@
-package validators;
-
-public interface RequiredValidator {
-    default void requiredCheck(String str, RuntimeException e) {
-        if (str == null || str.isBlank()) {
-            throw e;
-        }
-    }
-}
