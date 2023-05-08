73fc96dd07c7243d894e3b604e7d0cbb22921afb
yonggyo1125 <yonggyo00@kakao.com>
2023-05-08 17:33:26 +0900

    소스 업로드

--- tdd2/src/main/java/controllers/members/LoginServlet.java
+++ tdd2/src/main/java/controllers/members/LoginServlet.java
@@ -1,5 +1,8 @@
 package controllers.members;
 
+import models.member.LoginService;
+import models.member.ServiceManager;
+
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -7,6 +10,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+
+import static commons.MessageUtils.*;
 
 @WebServlet("/member/login")
 public class LoginServlet extends HttpServlet {
@@ -19,6 +24,17 @@ public class LoginServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        try {
+            ServiceManager manager = new ServiceManager();
+            LoginService service = manager.getLoginService();
+            service.login(req);
 
+            String url = req.getContextPath() + "/index.jsp";
+            go(resp, url, "parent");
+
+        } catch (RuntimeException e) {
+            alertError(resp, e);
+            e.printStackTrace();
+        }
     }
 }
